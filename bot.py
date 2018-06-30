7002f0e437c1a83d326ef6af44b2e924c3eed636
Pointless <MindScriptMC@gmail.com>
2018-06-30 21:43:25 +0100

    Gitacora saved 1 changed files

--- bot.py
+++ bot.py
@@ -140,7 +140,7 @@ async def cryptocurrency(ctx,coin:str=None):
     if coin:
         scryptocurrency=discord.Embed(title='Cryptocurrency',description='Information about the cryptocurrency, {}.'.format(str(coin)),color=0x00FF00)
         scryptocurrency.set_author(name=f'{ctx.message.author.display_name}', icon_url=f'{ctx.message.author.avatar_url}')
-        scryptocurrency.add_field(name='Price',value=json['RAW'][str(coin)]['USD']['PRICE'])
+        scryptocurrency.add_field(name='Price',value=json['D'][str(coin)]['USD']['PRICE'])
         return await bot.say(embed=scryptocurrency)
 
 '''
