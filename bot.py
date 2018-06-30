e6998e6309407ac4651c8087877224c194a16555
Pointless <MindScriptMC@gmail.com>
2018-06-30 11:41:40 +0100

    ok

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 import os
 from os import *
 
-bot = commands.Bot(command_prefix=commands.when_mentioned_or('!'), description='The one, and only: Botless, created by Pointless#1278.', self_bot=False, pm_help=None)
+bot = commands.Bot(command_prefix='!', description='The one, and only: Botless, created by Pointless#1278.', self_bot=False, pm_help=None)
 bot.remove_command('help')
 
 @bot.event
