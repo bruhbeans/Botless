f72d481bfcc828c9b291e13ee7a64b4da194a568
Pointless <MindScriptMC@gmail.com>
2018-06-30 11:54:08 +0100

    ok

--- bot.py
+++ bot.py
@@ -2,7 +2,6 @@ import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
 import os
-from os import *
 
 bot = commands.Bot(command_prefix='!', description='The one, and only: Botless, created by Pointless#1278.', self_bot=False, pm_help=None)
 bot.remove_command('help')
@@ -12,10 +11,9 @@ async def on_ready():
     print('Bot online!')
     print('Name: ' + str(bot.user))
     print('ID: ' + str(bot.user.id))
-    bot.send_message(destination='431973452677775363',content='Hey!')
 
 @bot.command(pass_context=True)
 async def test(ctx):
-    bot.say('Hey there!')
+    await bot.say('Hey there!')
 
 bot.run(os.environ.get('TOKEN'))
