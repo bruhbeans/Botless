657ac3a263eac541e126897ab5ad21c4d1035467
Pointless <MindScriptMC@gmail.com>
2018-06-30 11:19:35 +0100

    oof

---
+++ bot.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+from os import *
+
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('!'), description='The one, and only: Botless, created by Pointless#1278.', self_bot=False, pm_help=None)
+bot.remove_command('help')
+
+@bot.event
+async def on_ready():
+    print('Bot online!')
+    print('Name: ' + str(bot.user))
+    print('ID: ' + str(bot.user.id))
+
+bot.run(os.environ.get('TOKEN'))
