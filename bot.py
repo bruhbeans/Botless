21d42318e56921a690a34fb7a08b0f0a64b7dbbb
Pointless <MindScriptMC@gmail.com>
2018-06-30 20:44:05 +0100

    Gitacora saved 1 changed files

--- bot.py
+++ bot.py
@@ -138,7 +138,7 @@ async def cryptocurrency(ctx,coin:str=None):
         ncryptocurrency.set_author(name=f'{ctx.message.author.display_name}', icon_url=f'{ctx.message.author.avatar_url}')
         return await bot.say(embed=ncryptocurrency)
     if coin:
-        scryptocurrency=discord.Embed(title='Cy',description='You don\'t have permission to give roles to members!',color=0xFF0000)
+        scryptocurrency=discord.Embed(title='Cr',description='You don\'t have permission to give roles to members!',color=0xFF0000)
         scryptocurrency.set_author(name=f'{ctx.message.author.display_name}', icon_url=f'{ctx.message.author.avatar_url}')
         return await bot.say(embed=scryptocurrency)
     await bot.say(json['RAW'][str(coin)]['USD']['PRICE'])
