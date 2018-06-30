e7cc71b5534cd136e6fe6a96091a8fd745d78c71
Pointless <MindScriptMC@gmail.com>
2018-06-30 15:17:05 +0100

    oof

--- bot.py
+++ bot.py
@@ -173,4 +173,131 @@ async def takerole(ctx, member: discord.Member, *, role: discord.Role = None):
         else:
             pass
 
+'''
+'##::::'##::'#######::'########::'########:'########:::::'###::::'########:'####::'#######::'##::: ##:                          
+ ###::'###:'##.... ##: ##.... ##: ##.....:: ##.... ##:::'## ##:::... ##..::. ##::'##.... ##: ###:: ##:                          
+ ####'####: ##:::: ##: ##:::: ##: ##::::::: ##:::: ##::'##:. ##::::: ##::::: ##:: ##:::: ##: ####: ##:                          
+ ## ### ##: ##:::: ##: ##:::: ##: ######::: ########::'##:::. ##:::: ##::::: ##:: ##:::: ##: ## ## ##:                          
+ ##. #: ##: ##:::: ##: ##:::: ##: ##...:::: ##.. ##::: #########:::: ##::::: ##:: ##:::: ##: ##. ####:                          
+ ##:.:: ##: ##:::: ##: ##:::: ##: ##::::::: ##::. ##:: ##.... ##:::: ##::::: ##:: ##:::: ##: ##:. ###:                          
+ ##:::: ##:. #######:: ########:: ########: ##:::. ##: ##:::: ##:::: ##::::'####:. #######:: ##::. ##:                          
+..:::::..:::.......:::........:::........::..:::::..::..:::::..:::::..:::::....:::.......:::..::::..::   '''
+@bot.command(pass_context=True,brief='Kick members.')
+async def kick(ctx, member : discord.Member=None,*, reason='The kick hammer has spoken!'):
+    '''!kick <member> <reason>'''
+    if not ctx.message.author.server_permissions.kick_members:
+        pkick=discord.Embed(title='Error',description='You don\'t have permission to kick members!',color=0xFF0000)
+        pkick.set_author(name=f'{ctx.message.author.display_name}', icon_url=f'{ctx.message.author.avatar_url}')
+        return await bot.say(embed=pkick)
+    if not member:
+        mkick=discord.Embed(title='Error',description='You must specify a member!',color=0xFF0000)
+        mkick.set_author(name=f'{ctx.message.author.display_name}', icon_url=f'{ctx.message.author.avatar_url}')
+        return await bot.say(embed=mkick)
+    if not reason:
+        rkick=discord.Embed(title='Error',description='You must specify a reason!',color=0xFF0000)
+        rkick.set_author(name=f'{ctx.message.author.display_name}', icon_url=f'{ctx.message.author.avatar_url}')
+        return await bot.say(embed=rkick)
+    try:
+        await bot.kick(member)
+    except Exception as e:
+        if 'Privilege is too low' in str(e):
+            ekick=discord.Embed(title='Error',description='The person you are trying to kick has high permissions.',color=0xFF0000)
+            ekick.set_author(name=f'{ctx.message.author.display_name}', icon_url=f'{ctx.message.author.avatar_url}')
+            return await bot.say(embed=ekick)
+        else:
+            pass
+    skick=discord.Embed(title='Kick',description=f'{ctx.message.author.mention} has kicked {member.name}, because: {reason}!',color=0x00FF00)
+    skick.set_author(name=f'{ctx.message.author.display_name}', icon_url=f'{ctx.message.author.avatar_url}')
+    await bot.say(embed=skick)
+    return await bot.send_message(member, f'You have been kicked from {discord.Server.name} by {ctx.message.author.mention}, because {reason}!', tts=True) 
+
+@bot.command(pass_context=True,brief='Ban members.')
+async def ban(ctx, member : discord.Member=None,*, reason='The ban hammer has spoken!'):
+    '''!ban <member> <reason>'''
+    if not ctx.message.author.server_permissions.kick_members:
+        pban=discord.Embed(title='Error',description='You don\'t have permission to ban members!',color=0xFF0000)
+        pban.set_author(name=f'{ctx.message.author.display_name}', icon_url=f'{ctx.message.author.avatar_url}')
+        return await bot.say(embed=pban)
+    if not member:
+        mban=discord.Embed(title='Error',description='You must specify a member!',color=0xFF0000)
+        mban.set_author(name=f'{ctx.message.author.display_name}', icon_url=f'{ctx.message.author.avatar_url}')
+        return await bot.say(embed=mban)
+    if not reason:
+        rban=discord.Embed(title='Error',description='You must specify a reason!',color=0xFF0000)
+        rban.set_author(name=f'{ctx.message.author.display_name}', icon_url=f'{ctx.message.author.avatar_url}')
+        return await bot.say(embed=rban)
+    try:
+        await bot.ban(member)
+    except Exception as e:
+        if 'Privilege is too low' in str(e):
+            eban=discord.Embed(title='Error',description='The person you are trying to ban has high permissions.',color=0xFF0000)
+            eban.set_author(name=f'{ctx.message.author.display_name}', icon_url=f'{ctx.message.author.avatar_url}')
+            return await bot.say(embed=eban)
+        else:
+            pass
+    sban=discord.Embed(title='Ban',description=f'{ctx.message.author.mention} has banned {member.name}, because: {reason}!',color=0x00FF00)
+    sban.set_author(name=f'{ctx.message.author.display_name}', icon_url=f'{ctx.message.author.avatar_url}')
+    await bot.say(embed=sban)
+    return await bot.send_message(member, f'You have been banned from {discord.Server.name} by {ctx.message.author.mention}, because {reason}!', tts=True) 
+
+@bot.command(pass_context=True,brief='Unban members.')
+async def unban(ctx, member : discord.Member=None,*, reason='The unban hammer has spoken!'):
+    '''!unban <member> <reason>'''
+    await bot.say('May not work currently, but I\'ll give it a go!')
+    if not ctx.message.author.server_permissions.kick_members:
+        punban=discord.Embed(title='Error',description='You don\'t have permission to unban members!',color=0xFF0000)
+        punban.set_author(name=f'{ctx.message.author.display_name}', icon_url=f'{ctx.message.author.avatar_url}')
+        return await bot.say(embed=punban)
+    if not member:
+        munban=discord.Embed(title='Error',description='You must specify a member!',color=0xFF0000)
+        munban.set_author(name=f'{ctx.message.author.display_name}', icon_url=f'{ctx.message.author.avatar_url}')
+        return await bot.say(embed=munban)
+    if not reason:
+        runban=discord.Embed(title='Error',description='You must specify a reason!',color=0xFF0000)
+        runban.set_author(name=f'{ctx.message.author.display_name}', icon_url=f'{ctx.message.author.avatar_url}')
+        return await bot.say(embed=runban)
+    try:
+        await bot.unban(discord.Server,member)
+    except Exception as e:
+        if 'Privilege is too low' in str(e):
+            eunban=discord.Embed(title='Error',description='The person you are trying to unban has high permissions.',color=0xFF0000)
+            eunban.set_author(name=f'{ctx.message.author.display_name}', icon_url=f'{ctx.message.author.avatar_url}')
+            return await bot.say(embed=eunban)
+        else:
+            pass
+    sunban=discord.Embed(title='Unban',description=f'{ctx.message.author.mention} has unbanned {member.name}, because: {reason}!',color=0x00FF00)
+    sunban.set_author(name=f'{ctx.message.author.display_name}', icon_url=f'{ctx.message.author.avatar_url}')
+    await bot.say(embed=unban)
+    return await bot.send_message(member, f'You have been unbanned from {discord.Server.name} by {ctx.message.author.mention}, because {reason}!', tts=True) 
+
+@bot.command(pass_context=True,brief='Ban then unban someone to clear all messages within 7 days.')
+async def softban(ctx, member : discord.Member=None,*, reason='The softban hammer has spoken!'):
+    '''!softban <member> <reason>'''
+    if not ctx.message.author.server_permissions.kick_members:
+        psoftban=discord.Embed(title='Error',description='You don\'t have permission to softban members!',color=0xFF0000)
+        psoftban.set_author(name=f'{ctx.message.author.display_name}', icon_url=f'{ctx.message.author.avatar_url}')
+        return await bot.say(embed=psoftban)
+    if not member:
+        msoftban=discord.Embed(title='Error',description='You must specify a member!',color=0xFF0000)
+        msoftban.set_author(name=f'{ctx.message.author.display_name}', icon_url=f'{ctx.message.author.avatar_url}')
+        return await bot.say(embed=msoftban)
+    if not reason:
+        rsoftban=discord.Embed(title='Error',description='You must specify a reason!',color=0xFF0000)
+        rsoftban.set_author(name=f'{ctx.message.author.display_name}', icon_url=f'{ctx.message.author.avatar_url}')
+        return await bot.say(embed=rsoftban)
+    try:
+        await bot.ban(member, delete_message_days=7)
+        await bot.unban(discord.Server,member)
+    except Exception as e:
+        if 'Privilege is too low' in str(e):
+            esoftban=discord.Embed(title='Error',description='The person you are trying to softban has high permissions.',color=0xFF0000)
+            esoftban.set_author(name=f'{ctx.message.author.display_name}', icon_url=f'{ctx.message.author.avatar_url}')
+            return await bot.say(embed=esoftban)
+        else:
+            pass
+    ssoftban=discord.Embed(title='Softban',description=f'{ctx.message.author.mention} has softbanned {member.name}, because: {reason}!',color=0x00FF00)
+    ssoftban.set_author(name=f'{ctx.message.author.display_name}', icon_url=f'{ctx.message.author.avatar_url}')
+    await bot.say(embed=ssoftban)
+    return await bot.send_message(member, f'You have been softbanned from {discord.Server.name} by {ctx.message.author.mention}, because {reason}!', tts=True) 
+
 bot.run(os.environ.get('TOKEN'))
