a5f648d42f9553176e028794a840fc663aa3a6e1
Pointless <MindScriptMC@gmail.com>
2018-06-30 23:07:18 +0100

    Pointless changed 1

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import os
 import time
 import math
 import requests
-impor
+import 
 import psutil
 
 bot = commands.Bot(command_prefix=commands.when_mentioned_or('!'), description='The one, and only: Botless, created by Pointless#1278.', self_bot=False)
