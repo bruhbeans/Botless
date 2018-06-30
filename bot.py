4ddcd4e6a39aad3bf820b63b969b1f0f2f746f0b
Pointless <MindScriptMC@gmail.com>
2018-06-30 22:09:52 +0100

    Gitacora saved 1 changed files

--- bot.py
+++ bot.py
@@ -142,7 +142,7 @@ async def cryptocurrency(ctx,coin:str=None):
         scryptocurrency.set_author(name=f'{ctx.message.author.display_name}', icon_url=f'{ctx.message.author.avatar_url}')
         scryptocurrency.add_field(name='Price',value=json['DISPLAY'][str(coin)]['USD']['PRICE'])
         scryptocurrency.add_field(name='Highest Price Today',value=json['DISPLAY'][str(coin)]['USD']['HIGHDAY']
-        scryptocurrency.add_field(name='Supply',value=json['DISPLAY'][str(coin)]['USD']['LOWDAY']
+        scryptocurrency.add_field(name='Lowe',value=json['DISPLAY'][str(coin)]['USD']['LOWDAY']
         scryptocurrency.add_field(name='Last Updated',value=json['DISPLAY'][str(coin)]['USD']['LASTUPDATE'])
         scryptocurrency.add_field(name='Supply',value=json['DISPLAY'][str(coin)]['USD']['SUPPLY']
 
