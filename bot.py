c000bd3626355bce50ec7d9dfbc57488b6601be5
Pointless <MindScriptMC@gmail.com>
2018-06-30 12:04:28 +0100

    ok

--- bot.py
+++ bot.py
@@ -3,17 +3,25 @@ from discord.ext.commands import Bot
 from discord.ext import commands
 import os
 
-bot = commands.Bot(command_prefix='!', description='The one, and only: Botless, created by Pointless#1278.', self_bot=False, pm_help=None)
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('!'), description='The one, and only: Botless, created by Pointless#1278.', self_bot=False, pm_help=None)
 bot.remove_command('help')
 
 @bot.event
 async def on_ready():
     print('Bot online!')
-    print('Name: ' + str(bot.user))
-    print('ID: ' + str(bot.user.id))
+    print('Name: {}'.format(str(bot.user)))
+    print('ID: {}'.format(str(bot.user.id)))
+    print('Invite Link: https://discordapp.com/oauth2/authorize?client_id=462562571229200384&scope=bot&permissions=2146958591')
 
-@bot.command(pass_context=True)
-async def test(ctx):
-    await bot.say('Hey there!')
+@bot.command(pass_context=True,aliases=['latency','pong'])
+async def ping(ctx):
+    ptime = time.time()
+    embed = discord.Embed(Title = 'Ping', color = 0x0000FF)
+    embed.add_field(name = 'Pong!', value = 'Calculating...')
+    ping3 = await bot.say(embed=embed)
+    ping2 = time.time() - ptime
+    ping1 = discord.Embed(Title = 'Ping', color = 0x0000FF)
+    ping1.add_field(name = 'Pong!', value = "{} seconds".format(ping2))
+    await bot.edit_message(ping3, embed=ping1)
 
 bot.run(os.environ.get('TOKEN'))
