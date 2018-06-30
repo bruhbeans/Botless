1179d39bfeff24332cdf839f7fade20b65d37bcf
Pointless <MindScriptMC@gmail.com>
2018-06-30 20:23:00 +0100

    Gitacora saved 1 changed files

--- bot.py
+++ bot.py
@@ -132,7 +132,7 @@ async def info(ctx):
 async def cryptocurrency(ctx,coin:str=None):
     '''Find out cryptocurrency rates.\nUsage: !cryptocurrency\nAliases: !cc\nPermissions: None'''
     r = requests.get('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=' + str(coin) + '&tsyms=USD')
-    json ()
+    json
     await bot.say(r.json()['DISPLAY'][str(coin)]['USD']['PRICE'])
 
 '''
