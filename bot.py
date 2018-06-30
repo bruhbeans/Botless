c68fcbe162cf0168c1cb271a7bab991af3fe0f2c
Pointless <MindScriptMC@gmail.com>
2018-06-30 20:42:06 +0100

    Gitacora saved 1 changed files

--- bot.py
+++ bot.py
@@ -134,7 +134,7 @@ async def cryptocurrency(ctx,coin:str=None):
     r = requests.get('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=' + (coin) + '&tsyms=USD')
     json = r.json()
     if coin == None:
-        ncoin
+        nco
     await bot.say(json['RAW'][str(coin)]['USD']['PRICE'])
 
 '''
