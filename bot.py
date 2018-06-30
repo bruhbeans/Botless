13aae3d683def08c3ec05a1f4412f97c1ae9309f
Pointless <MindScriptMC@gmail.com>
2018-06-30 11:28:21 +0100

    ok

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
+import os
 from os import *
 
 bot = commands.Bot(command_prefix=commands.when_mentioned_or('!'), description='The one, and only: Botless, created by Pointless#1278.', self_bot=False, pm_help=None)
