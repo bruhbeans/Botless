d5e884d0251decaa505aaa93407830395336d361
Pointless <MindScriptMC@gmail.com>
2018-06-30 20:43:19 +0100

    Gitacora saved 1 changed files

--- bot.py
+++ bot.py
@@ -135,7 +135,7 @@ async def cryptocurrency(ctx,coin:str=None):
     json = r.json()
     if coin == None:
         ncryptocurrency=discord.Embed(title='Error',description='Specify the cryptocurrency symbol!',color=0xFF0000)
-        ngiverole.set_author(name=f'{ctx.message.author.display_name}', icon_url=f'{ctx.message.author.avatar_url}')
+        ncry.set_author(name=f'{ctx.message.author.display_name}', icon_url=f'{ctx.message.author.avatar_url}')
         return await bot.say(embed=ngiverole)
     if coin:
         s
