c36070e84548c570f6a027871052256d0f3769ec
P01nt-Less <mindscriptmc@gmail.com>
2018-07-01 14:08:45 +0100

    ok

--- bot.py
+++ bot.py
@@ -180,7 +180,7 @@ async def add(ctx, a,b):
         nadd2=discord.Embed(title='Error',description='Specify the second number!',color=0xFF0000)
         nadd2.set_author(name=f'{ctx.message.author.display_name}', icon_url=f'{ctx.message.author.avatar_url}')
         return await bot.say(embed=nadd2)
-    sadd=discord.Embed(title='Error',description=str(number) + str(number2),color=0xFF0000)
+    sadd=discord.Embed(title='Error',description=str(a) + str(b),color=0xFF0000)
     sadd.set_author(name=f'{ctx.message.author.display_name}', icon_url=f'{ctx.message.author.avatar_url}')
     return await bot.say(embed=sadd)
 '''
