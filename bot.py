931d8c118ea3c8270d83da599202848e648375c2
P01nt-Less <mindscriptmc@gmail.com>
2018-07-02 22:06:19 +0100

    oofofo

--- bot.py
+++ bot.py
@@ -139,8 +139,9 @@ async def suggest(ctx, *, phrase):
         await bot.say(embed=nsuggest)
     osuggest = discord.Embed(title='Suggest',description=phrase,color=0x00FF00)
     osuggest.set_author(name=f'{ctx.message.author.display_name}', icon_url=f'{ctx.message.author.avatar_url}')
-    await bot.add_reaction(phrase, '👍')
-    await bot.add_reaction(phrase, '👎')
+    messagee = bot.say(embed=osuggest)
+    await messagee.add_reaction(phrase, '👍')
+    await messagee.add_reaction(phrase, '👎')
     if phrase:
         for server in bot.servers:
             for channel in server.channels:
